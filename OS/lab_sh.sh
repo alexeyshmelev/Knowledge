2d5e46a3c66f9e9d860846e9d8fe9895f565c31e
Alexey Shmelev <60858323+alexeyshmelev@users.noreply.github.com>
2021-12-16 18:28:14 +0300

    Update lab_sh.sh

--- OS/lab_sh.sh
+++ OS/lab_sh.sh
@@ -1,22 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 echo Lab 1. Var 23.
 date
 
-let count=0
+count=0
 
 handl(){
-let count++
+count=$((count+1))
 if [ $count -eq 2 ]
 then
-find ../Homework_2/ -maxdepth 1 -size +24b -atime -5 -ls > some_file
-awk 'BEGIN{total=0} {total += $2} END{print "\nTotal size: " total}' < some_file
-exit 0
+awk 'BEGIN{total=0} {total += $2} END{print "\nTotal size: " total}' some_file
 fi
 }
 
-trap "handl" SIGINT
+trap "handl" 2
+text=`find ../Homework_2/ -maxdepth 1 -size +24b -atime -5 -ls`
+echo $text > some_file
 sleep 2
 sleep 2
 sleep 2
-find ../Homework_2/ -maxdepth 1 -size +24b -atime -5 -ls > sime_file
-awk 'BEGIN{print "Selected files:"} {print $11}' < some_file
+awk 'BEGIN{print "Selected files:"} {print $11}' some_file
