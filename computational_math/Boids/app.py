bdd64c9b4ae175ccffaef5a1effde5a4c426758f
Alexey Shmelev <60858323+alexeyshmelev@users.noreply.github.com>
2022-02-08 00:00:12 +0300

    Add files via upload

--- computational_math/Boids/app.py
+++ computational_math/Boids/app.py
@@ -2,75 +2,41 @@ import numpy as np
 import ffmpeg
 from datetime import datetime
 from vispy import app, scene
+from vispy.scene.visuals import Text
 from functions import *
-import time
 import os
-from threading import Thread
+from numba import cuda
 os.environ['CUPY_ACCELERATORS'] = 'cub,cutensor'
 
-from numba import cuda
-print(cuda.gpus)
-
-#%%
 
 app.use_app('pyglet')
-
 asp = 16 / 9
 h = 720
 w = int(asp * h)
-
-canvas = scene.SceneCanvas(keys='interactive',
-                           show=True,
-                           size=(w, h))
-
+canvas = scene.SceneCanvas(keys='interactive', show=True, size=(w, h))
 view = canvas.central_widget.add_view()
-view.camera = scene.PanZoomCamera(rect=(0, 0, asp, 1),
-                                  aspect=1)
-
-
+view.camera = scene.PanZoomCamera(rect=(0, 0, asp, 1), aspect=1)
 face = "Times New Roman"
-#%%
-
-video = False
-N = 10000
+video = True
+frames = 0
+counter_mod = 0
+N = 100
 dt = 0.05
 perception = 1./20
 vrange = np.array([0.05, 0.1])
 arange = np.array([0., 1.0])
 
-coeffs = np.array([1.0,  # alignment
-                   1.0,  # cohesion
-                   0.05,  # separation
-                   0.001,  # walls
-                   0.0   # noise
-                   ], dtype=np.float64)
+t1 = Text("text", parent=canvas.scene, color='orange')
+t1.font_size = 18
+t1.pos = canvas.size[0] // 2, canvas.size[1] - 20
 
 # x, y, vx, vy, ax, ay
-boids = np.zeros((N, 6), dtype=np.float32)
-D = cuda.to_device(np.zeros((N, N), dtype=np.float32))
+boids = np.zeros((N, 6), dtype=np.float64)
 init_boids(boids, asp, vrange)
-# nb0 = calc_neighbors(boids, perception)
+arrows = scene.Arrow(arrows=directions(boids), arrow_color=(1, 1, 1, 1), arrow_size=5, connect='segments', parent=view.scene)
+scene.Line(pos=np.array([[0, 0], [asp, 0], [asp, 1], [0, 1], [0, 0]]),color=(0, 1, 0, 1), connect='strip', method='gl', parent=view.scene)
+scene.GridLines((1/6, 1/6), parent=view.scene)
 
-arrows = \
-scene.Arrow(arrows=directions(boids),
-            arrow_color=(1, 1, 1, 1),
-            arrow_size=5,
-            connect='segments',
-            parent=view.scene
-            )
-
-scene.Line(pos=np.array([[0, 0],
-                         [asp, 0],
-                         [asp, 1],
-                         [0, 1],
-                         [0, 0]
-                         ]),
-           color=(0, 1, 0, 1),
-           connect='strip',
-           method='gl',
-           parent=view.scene
-           )
-#%%
 
 if video:
     fname = f"boids_{datetime.now().strftime('%Y%m%d_%H%M%S')}.mp4"
@@ -83,40 +49,52 @@ if video:
             .overwrite_output()
             .run_async(pipe_stdin=True)
     )
-#%%
 
-threadsperblock = 64
-blockspergrid = math.ceil(N / threadsperblock)
-
-M = cuda.to_device(np.zeros((N, N), dtype=np.float32))
-left = cuda.to_device(np.zeros(N, dtype=np.float32))
-right = cuda.to_device(np.zeros(N, dtype=np.float32))
-bottom = cuda.to_device(np.zeros(N, dtype=np.float32))
-top = cuda.to_device(np.zeros(N, dtype=np.float32))
-ax = cuda.to_device(np.zeros(N, dtype=np.float32))
-ay = cuda.to_device(np.zeros(N, dtype=np.float32))
-wa = cuda.to_device(np.zeros((N, 2), dtype=np.float32))
-accels = cuda.to_device(np.zeros((N, 5, 2), dtype=np.float32))
-tmp = cuda.to_device(np.zeros((N, N, 2), dtype=np.float32))
-counter = cuda.to_device(np.zeros(N, dtype=np.float32))
-counter2d = cuda.to_device(np.zeros((N, 2), dtype=np.float32))
+threadsperblock = (32, 32)
+blockspergrid_x = math.ceil(N / threadsperblock[0])
+blockspergrid_y = math.ceil(N / threadsperblock[1])
+blockspergrid = (blockspergrid_x, blockspergrid_y)
+D = cuda.to_device(np.zeros((N, N), dtype=np.float64))
+left = cuda.to_device(np.zeros(N, dtype=np.float64))
+right = cuda.to_device(np.zeros(N, dtype=np.float64))
+bottom = cuda.to_device(np.zeros(N, dtype=np.float64))
+top = cuda.to_device(np.zeros(N, dtype=np.float64))
+ax = cuda.to_device(np.zeros(N, dtype=np.float64))
+ay = cuda.to_device(np.zeros(N, dtype=np.float64))
+wa = cuda.to_device(np.zeros((N, 2), dtype=np.float64))
+accels = cuda.to_device(np.zeros((N, 5, 2), dtype=np.float64))
+tmp = cuda.to_device(np.zeros((N, N, 2), dtype=np.float64))
+counter = cuda.to_device(np.zeros(N, dtype=np.float64))
+counter2d = cuda.to_device(np.zeros((N, 2), dtype=np.float64))
 
 
 def update(event):
-    global process, boids, D
-    # boids = cuda.to_device(boids)
-    new_simulate[blockspergrid, threadsperblock](boids, D, M, perception, asp, coeffs, left, right, bottom, top, ax, ay, wa, accels, tmp, counter, counter2d)
+    global process, boids, D, accels, frames, counter_mod
+    frames += 1
+    counter_mod += 1
+    coeffs = np.array([1,  # alignment
+                       0.1,  # cohesion
+                       math.sin(counter_mod/100)/10,  # separation
+                       0.001,  # walls
+                       0.  # noise
+                       ], dtype=np.float64)
+    coeffs = cuda.to_device(coeffs)
+    boids = cuda.to_device(boids)
+    new_simulate[blockspergrid, threadsperblock](boids, D, perception, asp, coeffs, left, right, bottom, top, wa, accels, tmp, counter, counter2d)
+    boids = boids.copy_to_host()
+    coeffs = coeffs.copy_to_host()
     propagate(boids, dt, vrange)
     periodic_walls(boids, asp)
     arrows.set_data(arrows=directions(boids))
+    t1.text = f"FPS (GPU): {canvas.fps:0.1f}, coefficients: {coeffs[0]}, {coeffs[1]}, {coeffs[2]}, {coeffs[3]}, {coeffs[4]}, number of boids: {N}"
     if video:
         frame = canvas.render(alpha=False)
         process.stdin.write(frame.tobytes())
     else:
         canvas.update(event)
-    # print(f"{canvas.fps:0.1f}")
+    if frames > 3600:
+        exit()
 
-#%%
 
 timer = app.Timer(interval=0, start=True, connect=update)
 
--- computational_math/Boids/functions.py
+++ computational_math/Boids/functions.py
@@ -1,32 +1,42 @@
 import numpy as np
 from scipy.spatial.distance import cdist
-from numba import njit, prange, cuda, jit
+from numba import njit, cuda, jit
 import math
 import time
 
 
-
 @cuda.jit
-def new_simulate(boids, D, M, perception, asp, coeffs, left, right, bottom, top, ax, ay, wa, accels, tmp, counter, counter2d):
-    # M, left, right, bottom, top, ax, ay, wa, accels, res - zeros matrix
-    # calc_dist
-    arr = boids[:, :2]
-    n = arr.shape[0]
-    i = cuda.grid(1)
-    if i < n:
-        for j in range(i):
-            counter[i] = ((arr[j, 0] - arr[i, 0]) ** 2 + (arr[j, 1] - arr[i, 1]) ** 2) ** 0.5
-            D[i, j] = counter[i]
-            D[j, i] = counter[i]
-    # check radius
-    M[:, :] = 0.
-    if i < n:
-        for j in range(n):
-            if D[i, j] < perception:
-                if i != j:
-                    M[i, j] = 1.
+def new_simulate(boids, D, perception, asp, coeffs, left, right, bottom, top, wa, accels, tmp, counter, counter2d):
+    """
+    Производит основную часть симуляции на GPU (все использованные переменные мы передаём заранее, т.к. прямым образом выделить новую память на GPU нельзя)
+
+    :param boids: массив, содержащий всю необходимую информацию о каждом boid
+    :param D: матрица расстояний
+    :param perception: радиус восприятия для каждого boid
+    :param asp: соотношение сторон
+    :param coeffs: коэффициенты для каждого тыпа ускорений
+    :param left: массив для хранения данных, учитывающихся при отталкивании от левой границы
+    :param right: массив для хранения данных, учитывающихся при отталкивании от правой границы
+    :param bottom: массив для хранения данных, учитывающихся при отталкивании от нижней границы
+    :param top: массив для хранения данных, учитывающихся при отталкивании от верхней границы
+    :param wa: изменения ускорений при отталкивании от границы
+    :param accels: массив со всеми ускорениями для каждого boid
+    :param tmp: массив для хранения промежуточных вычислений
+    :param counter: массив для хранения промеруточных вычислений
+    :param counter2d: массив для хранения промеруточных вычислений
+    :return: boids с именёнными параметрами ускорений как ссылку
+    """
+    n = boids.shape[0]
+    i, j = cuda.grid(2)
+
+    # pairwise distance matrix
+    if n > i != j < n:
+        D[i, j] = ((boids[j, 0] - boids[i, 0]) ** 2 + (boids[j, 1] - boids[i, 1]) ** 2) ** 0.5
+
+    cuda.syncthreads()
+
     # wall avoidance
-    if i < n:
+    if i < n and j == 0:
         left[i] = math.fabs(boids[i, 0])
         right[i] = math.fabs(asp - boids[i, 0])
         bottom[i] = math.fabs(boids[i, 1])
@@ -34,64 +44,119 @@ def new_simulate(boids, D, M, perception, asp, coeffs, left, right, bottom, top,
 
         wa[i, 0] = 1 / left[i] ** 2 - 1 / right[i] ** 2
         wa[i, 1] = 1 / bottom[i] ** 2 - 1 / top[i] ** 2
-    # range
-    accels[:, :, :] = 0
-    tmp[:, :, :] = 0
-    if i < n:
-        # alignment
-        mean_gpu(boids, M, i, n, 2, 3, accels, counter, counter2d)
-        # cohesion
-        mean_gpu(boids, M, i, n, 0, 1, accels, counter, counter2d)
-        # separation
-        separation_gpu(boids, M, i, n, tmp, D, accels, counter2d)
+        accels[i, :, :] = 0
 
+    cuda.syncthreads()
+
+    if i < n and j < n:
+        tmp[i, j, :] = 0
+
+    cuda.syncthreads()
+
+    # alignment
+    mean_gpu(boids, D, i, j, n, 2, 3, accels, counter, counter2d, perception)
+    cuda.syncthreads()
+    # # cohesion
+    mean_gpu(boids, D, i, j, n, 0, 1, accels, counter, counter2d, perception)
+    cuda.syncthreads()
+    # separation
+    separation_gpu(boids, D, i, j, n, accels, perception, counter2d, tmp)
+    cuda.syncthreads()
+
+    if i < n and j == 0:
         accels[i, 3, 0] = wa[i, 0]
         accels[i, 3, 1] = wa[i, 1]
-
+        accels[i, 4, 0] = asp / 2 - boids[i, 0]
+        accels[i, 4, 1] = 1 / 2 - boids[i, 1]
         boids[i, 4] = accels[i, 0, 0] * coeffs[0] + accels[i, 1, 0] * coeffs[1] + accels[i, 2, 0] * coeffs[2] + accels[i, 3, 0] * coeffs[3] + accels[i, 4, 0] * coeffs[4]
         boids[i, 5] = accels[i, 0, 1] * coeffs[0] + accels[i, 1, 1] * coeffs[1] + accels[i, 2, 1] * coeffs[2] + accels[i, 3, 1] * coeffs[3] + accels[i, 4, 1] * coeffs[4]
 
-
-@cuda.jit(device=True)
-def mean_gpu(boids, M, i, n, a, b, accels, counter, counter2d):
-    counter2d[i, 0] = 0
-    counter2d[i, 1] = 0
-    counter[i] = 0
-    for j in prange(n):
-        if M[i, j] == 1:
-            counter2d[i, 0] += boids[j, a]
-            counter2d[i, 1] += boids[j, b]
-            counter[i] += 1
-    counter2d[i, 0] /= counter[i]
-    counter2d[i, 1] /= counter[i]
-    if a == 2 and b == 3:
-        accels[i, 0, 0] = counter2d[i, 0] - boids[i, a]
-        accels[i, 0, 1] = counter2d[i, 1] - boids[i, b]
-    else:
-        accels[i, 1, 0] = counter2d[i, 0] - boids[i, 0]
-        accels[i, 1, 1] = counter2d[i, 1] - boids[i, 1]
+    cuda.syncthreads()
 
 
-@cuda.jit(device=True)
-def separation_gpu(boids, M, i, n, tmp, D, accels, counter2d):
-    counter2d[i, 0] = 0
-    counter2d[i, 1] = 0
-    for j in prange(n):
-        if M[i, j] == 1:
-            tmp[i, j, 0] = - boids[j, 0] + boids[i, 0]
-            tmp[i, j, 1] = - boids[j, 1] + boids[i, 1]
-            counter2d[i, 0] += tmp[i, j, 0] / D[i, j]
-            counter2d[i, 1] += tmp[i, j, 1] / D[i, j]
-    # for j in prange(n):
-    #     if M[i, j] == 1:
-    #         counter2d[i, 0] += tmp[i, j, 0] / D[i, j]
-    #         counter2d[i, 1] += tmp[i, j, 1] / D[i, j]
+@cuda.jit
+def mean_gpu(boids, D, i, j, n, a, b, accels, counter, counter2d, perception):
+    """
+    Вычисление среднего значение по всем скоростям или ускорениям для каждой координаты для тех boid, которые находятся в радиусе perception
 
-    accels[i, 2, 0], accels[i, 2, 1] = counter2d[i, 0], counter2d[i, 1]
+    :param boids: массив, содержащий всю необходимую информацию о каждом boid
+    :param D: матрица расстояний
+    :param i: поток по одному направлению в сетке
+    :param j: поток по другому направлению в сетке
+    :param n: количество boids
+    :param a: скорость или ускорение по x
+    :param b: скорость или ускорение по y
+    :param accels: массив со всеми ускорениями для каждого boid
+    :param counter: массив для хранения промеруточных вычислений
+    :param counter2d: массив для хранения промеруточных вычислений
+    :param perception: радиус восприятия для каждого boid
+    :return: всё изменяется по ссылке
+    """
+    if i < n and j == 0:
+        counter2d[i, 0] = 0
+        counter2d[i, 1] = 0
+        counter[i] = 0
+
+    cuda.syncthreads()
+
+    if i < n and j == 0:
+        for k in range(n): # т.к. atomic operations работают с двумерной сеткой потоков неправильно, они блокируют только либо для i либо для j
+            if 0 < D[k, i] < perception:
+                cuda.atomic.add(counter2d, (i, 0), boids[k, a])
+                cuda.atomic.add(counter2d, (i, 1), boids[k, b])
+                cuda.atomic.add(counter, i, 1)
+
+    if i < n and j == 0:
+        if counter[i] != 0:
+            counter2d[i, 0] /= counter[i]
+            counter2d[i, 1] /= counter[i]
+            if a == 2 and b == 3:
+                accels[i, 0, 0] = math.sin(counter2d[i, 0] - boids[i, a])
+                accels[i, 0, 1] = math.sin(counter2d[i, 1] - boids[i, b])
+            if a == 0 and b == 1:
+                accels[i, 1, 0] = counter2d[i, 0] - boids[i, a] ** 3
+                accels[i, 1, 1] = counter2d[i, 1] - boids[i, b] ** 3
 
 
-@njit
+@cuda.jit
+def separation_gpu(boids, D, i, j, n, accels, perception, counter2d, tmp):
+    """
+    Подсчёт составляющей ускорения, которая отвечает за рассредоточение boids
+
+    :param boids: массив, содержащий всю необходимую информацию о каждом boid
+    :param D: матрица расстояний
+    :param i: поток по одному направлению в сетке
+    :param j: поток по другому направлению в сетке
+    :param n: количество boids
+    :param accels: массив со всеми ускорениями для каждого boid
+    :param perception: радиус восприятия для каждого boid
+    :param counter2d: массив для хранения промеруточных вычислений
+    :param tmp: массив для хранения промеруточных вычислений
+    :return: всё изменяется по ссылке
+    """
+    if n > i != j < n:
+        if 0 < D[i, j] < perception:
+            tmp[i, j, 0] = (- math.sin(boids[j, 0]) + math.sin(boids[i, 0])) / D[i, j]
+            tmp[i, j, 1] = (- math.sin(boids[j, 1]) + math.sin(boids[i, 1])) / D[i, j]
+
+    cuda.syncthreads()
+
+    if i < n and j == 0:
+        for k in range(n):
+            cuda.atomic.add(accels, (i, 2, 0), tmp[i, k, 0])
+            cuda.atomic.add(accels, (i, 2, 1), tmp[i, k, 1])
+
+
+@njit(cache=True)
 def clip_mag(arr, low, high):
+    """
+    Ограничение скорости
+
+    :param arr: массив для всех boids
+    :param low: минимальная скорость
+    :param high: максимальная скорость
+    :return: всё изменяется по ссылке
+    """
     mag = np.sum(arr * arr, axis=1) ** 0.5
     mask = mag > 1e-16
     mag_cl = np.clip(mag[mask], low, high)
@@ -100,6 +165,14 @@ def clip_mag(arr, low, high):
 
 @njit(cache=True)
 def init_boids(boids, asp, vrange=(0., 1.), seed=0):
+    """
+
+    :param boids: массив для всех boids
+    :param asp: соотношение сторон
+    :param vrange: диапазон начальных скоростей
+    :param seed: seed
+    :return: всё изменяется по ссылке
+    """
     N = boids.shape[0]
     np.random.seed(seed)
     boids[:, 0] = np.random.rand(N) * asp
@@ -113,11 +186,25 @@ def init_boids(boids, asp, vrange=(0., 1.), seed=0):
 
 @njit(cache=True)
 def directions(boids):
+    """
+    Рисование стрелок
+
+    :param boids: массив для всех boids
+    :return: всё изменяется по ссылке
+    """
     return np.hstack((boids[:, :2] - boids[:, 2:4], boids[:, :2]))
 
 
 @njit(cache=True)
 def propagate(boids, dt, vrange):
+    """
+    Изменение скорости во времение
+
+    :param boids: массив для всех boids
+    :param dt: временной шаг
+    :param vrange: диапазон начальных скоростей
+    :return: всё изменяется по ссылке
+    """
     boids[:, :2] += dt * boids[:, 2:4] + 0.5 * dt ** 2 * boids[:, 4:6]
     boids[:, 2:4] += dt * boids[:, 4:6]
     clip_mag(boids[:, 2:4], vrange[0], vrange[1])
@@ -125,4 +212,11 @@ def propagate(boids, dt, vrange):
 
 @njit(cache=True)
 def periodic_walls(boids, asp):
+    """
+    Обработка вылета за стену
+
+    :param boids: массив для всех boids
+    :param asp: соотношение сторон
+    :return: всё изменяется по ссылке
+    """
     boids[:, :2] %= np.array([asp, 1.])
