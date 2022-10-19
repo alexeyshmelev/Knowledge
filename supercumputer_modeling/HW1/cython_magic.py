76781bc0b064fa7b3440e56544182bc71eeae2d9
Alexey Shmelev <justadocs@gmail.com>
2022-10-19 21:07:04 +0300

    Update cython_magic.py

--- supercumputer_modeling/HW1/cython_magic.py
+++ supercumputer_modeling/HW1/cython_magic.py
@@ -1,10 +1,10 @@
-from variant5_cython import max_force
+from code_cythone import max_force
 import numpy as np
 import os
 from time import time
 import timeit
 
-count_exper = 1
+counter = 1
 tt = []
 gf = []
 mf = []
@@ -15,13 +15,13 @@ for f in files:
     n = arr.shape[1]
 
     t = time()
-    for i in range(count_exper):
+    for i in range(counter):
         max_force(arr[0], arr[1], arr[2], arr[3])
     t = time() - t
     mforce = max_force(arr[0], arr[1], arr[2], arr[3])
 
-    tt.append(t / count_exper)
-    gf.append(((n*(n-1)/2) * 13 * count_exper)  / t)
+    tt.append(t / counter)
+    gf.append(((n*(n-1)/2) * 13 * counter)  / t)
     mf.append(mforce)
 np.savetxt('times_cython.txt', tt)
 np.savetxt('gflops_cython.txt', gf)
