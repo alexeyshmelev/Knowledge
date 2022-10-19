b3b7792f6fcf83c9c7e14379cb4639aee2e81c47
Alexey Shmelev <justadocs@gmail.com>
2022-10-19 20:54:53 +0300

    Update cython_magic.py

--- supercumputer_modeling/HW1/cython_magic.py
+++ supercumputer_modeling/HW1/cython_magic.py
@@ -9,22 +9,17 @@ tt = []
 gf = []
 mf = []
 
-files = sorted(os.listdir('arrays/arrays_py/'))
+files = sorted(os.listdir('/home/avshmelev/bash_scripts/sm'))
 for f in files:
-    arr = np.loadtxt('arrays/arrays_py/'+f)
+    arr = np.loadtxt('/home/avshmelev/bash_scripts/sm'+f)
     n = arr.shape[1]
 
-    # t = timeit.timeit('max_force(arr[0], arr[1], arr[2], arr[3])', number=count_exper, globals=globals())
     t = time()
     for i in range(count_exper):
         max_force(arr[0], arr[1], arr[2], arr[3])
     t = time() - t
     mforce = max_force(arr[0], arr[1], arr[2], arr[3])
 
-    print('Answer:', mforce)
-    print('Timing:', t, 'seconds')
-    print('Timing per run:', t / count_exper, 'seconds')
-    print('GFlops:', ((n*(n-1)/2) * 13 * count_exper)  / (t*1e9))
     tt.append(t / count_exper)
     gf.append(((n*(n-1)/2) * 13 * count_exper)  / t)
     mf.append(mforce)
