28224ff747611237cf252acf653385beb62199cb
Alexey Shmelev <justadocs@gmail.com>
2022-12-08 00:01:40 +0300

    Update laplace_loop.py

--- supercumputer_modeling/HW2/laplace_loop.py
+++ supercumputer_modeling/HW2/laplace_loop.py
@@ -6,10 +6,13 @@ import matplotlib.pyplot as plt
 flops = []
 
 for i in range(1, 33):
-    print(i)
     os.environ["OMP_NUM_THREADS"] = str(i)
-    res = subprocess.run(["./a.out"], input='300\n', capture_output=True, text=True, shell=True)
-    flops.append(float(res.stdout.split('\n')[-1].split(' ')[-1]))
+    arr_size = '220' #str(300 + 15 * (i-1)) if i <= 10 else '500'
+    res = subprocess.run(["./a.out"], input=f"{arr_size}\n", capture_output=True, text=True, shell=True)
+    n = float(res.stdout.split('\n')[-1].split(' ')[-1])
+    t = res.stdout.split('\n')[0].split(' ')[1]
+    print(f'num_threads: {i}, arr_size: {arr_size}, FLOPS: {n}, time: {t} sec')
+    flops.append(n)
 
 plt.clf()
 plt.plot(flops)
